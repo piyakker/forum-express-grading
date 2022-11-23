9447728d39ae5f9f4ce40a6fff7cfcbdff947b5f
piyakker <ericlien86@gamil.com>
2022-11-23 16:16:26 +0800

    add signup function

--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,12 @@ const express = require('express')
 const router = express.Router()
 const admin = require('./modules/admin')
 const restControllers = require('../controllers/restaurant-controllers')
+const userController = require('../controllers/user-controller')
 
 router.use('/admin', admin)
 
+router.get('/signup', userController.signUpPage)
+router.post('/signup', userController.signUp)
 router.get('/restaurants', restControllers.getRestaurants)
 router.use('/', (req, res) => { res.redirect('/restaurants') })
 
