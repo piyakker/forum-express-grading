61aeaad18e8f1d3ded77748a5f96abdd612a1e3f
piyakker <ericlien86@gamil.com>
2022-11-23 17:56:13 +0800

    add login function

--- routes/index.js
+++ routes/index.js
@@ -3,12 +3,22 @@ const router = express.Router()
 const admin = require('./modules/admin')
 const restControllers = require('../controllers/restaurant-controllers')
 const userController = require('../controllers/user-controller')
+const passport = require('../config/passport')
 
 const { generalErrorHandler } = require('../middleware/error-handler')
 router.use('/admin', admin)
 
 router.get('/signup', userController.signUpPage)
 router.post('/signup', userController.signUp)
+
+router.get('/signin', userController.signInPage)
+router.post('/signin', passport.authenticate('local', {
+  failureRedirect: '/signin',
+  failureFlash: true
+}), userController.signIn)
+
+router.get('/logout', userController.logout)
+
 router.get('/restaurants', restControllers.getRestaurants)
 router.use('/', (req, res) => { res.redirect('/restaurants') })
 
