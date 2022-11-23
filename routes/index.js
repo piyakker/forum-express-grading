b51c2b6a85c7a35b97460679e0726ff534ad68b7
piyakker <ericlien86@gamil.com>
2022-11-23 17:09:43 +0800

    add signup validation

--- routes/index.js
+++ routes/index.js
@@ -4,11 +4,14 @@ const admin = require('./modules/admin')
 const restControllers = require('../controllers/restaurant-controllers')
 const userController = require('../controllers/user-controller')
 
+const { generalErrorHandler } = require('../middleware/error-handler')
 router.use('/admin', admin)
 
 router.get('/signup', userController.signUpPage)
 router.post('/signup', userController.signUp)
 router.get('/restaurants', restControllers.getRestaurants)
 router.use('/', (req, res) => { res.redirect('/restaurants') })
+
+router.use('/', generalErrorHandler)
 
 module.exports = router
