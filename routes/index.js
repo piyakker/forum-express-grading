171f85e472f340fb520b4f3c69fdcb1fa0de7286
piyakker <ericlien86@gamil.com>
2022-11-30 11:44:00 +0800

    add restaurant page

--- routes/index.js
+++ routes/index.js
@@ -20,6 +20,7 @@ router.post('/signin', passport.authenticate('local', {
 
 router.get('/logout', userController.logout)
 
+router.get('/restaurants/:id', authenticated, restController.getRestaurant)
 router.get('/restaurants', authenticated, restController.getRestaurants)
 router.use('/', (req, res) => { res.redirect('/restaurants') })
 
