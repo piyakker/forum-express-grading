f25a397a98084678b1b17253a33ef66457796082
piyakker <ericlien86@gamil.com>
2022-11-30 11:21:27 +0800

    add delete category function

--- controllers/category-controller.js
+++ controllers/category-controller.js
@@ -30,6 +30,36 @@ const categoryController = {
       })
       .then(() => res.redirect('/admin/categories'))
       .catch(err => next(err))
+  },
+  deleteCategory: (req, res, next) => {
+    // promise.all
+    // 找到所有餐廳的分類是req.params.id的
+    // 找到該分類
+    // 餐廳的分類改設為 (未分類)
+    // 刪除該分類
+    // return Promise.all([
+    //   Restaurant.findAll({ where: { categoryId: req.params.id } }),
+    //   Category.findOne({ where: { id: req.params.id } })
+    // ])
+    //   .then(([restaurants, category]) => {
+    //     if (!category) throw new Error("Category didn't exist!")
+    //     if (!restaurants.length) {
+    //       return category.destroy()
+    //     }
+    //     return Promise.all([
+    //       restaurants.map(restaurant => restaurant.removeCategory(restaurant.Category)),
+    //       category.destroy()
+    //     ])
+    //   })
+    //   .then(() => res.redirect('/admin/categories'))
+    //   .catch(err => next(err))
+    return Category.findByPk(req.params.id)
+      .then(category => {
+        if (!category) throw new Error("Category didn't exist!")
+        return category.destroy()
+      })
+      .then(() => res.redirect('/admin/categories'))
+      .catch(err => next(err))
   }
 }
 module.exports = categoryController
