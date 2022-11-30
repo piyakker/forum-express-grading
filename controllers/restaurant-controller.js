a203eabe5c9a9d89e4fc776afe535c0e2572d0b2
piyakker <ericlien86@gamil.com>
2022-11-30 15:35:43 +0800

    show comments on restaurant page

--- controllers/restaurant-controller.js
+++ controllers/restaurant-controller.js
@@ -1,4 +1,4 @@
-const { Restaurant, Category } = require('../models')
+const { Restaurant, Category, Comment, User } = require('../models')
 const { getOffset, getPagination } = require('../helpers/pagination-helper')
 
 const restaurantController = {
@@ -38,10 +38,15 @@ const restaurantController = {
   },
   getRestaurant: (req, res, next) => {
     return Restaurant.findByPk(req.params.id, {
-      include: Category,
+      include: [
+        Category,
+        { model: Comment, include: User }
+      ],
+      order: [[Comment, 'id', 'desc']], // 新的評論在上面
       nest: true
     })
       .then(restaurant => {
+        console.log(restaurant.toJSON())
         if (!restaurant) throw new Error("Restaurant didn't exist!")
         return restaurant.increment('viewCounts', { by: 1 })
       })
