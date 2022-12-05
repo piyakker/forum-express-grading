4f24c165400332402b591bd520c93dae88aaf7ca
piyakker <ericlien86@gamil.com>
2022-12-05 21:36:01 +0800

    modify api unauthorize response

--- controllers/pages/user-controller.js
+++ controllers/pages/user-controller.js
@@ -1,6 +1,5 @@
-const bcrypt = require('bcryptjs')
-// const db = require('../models')
 const { User, Comment, Restaurant, Favorite, Like, Followship } = require('../../models')
+const userServices = require('../../services/user-services')
 const { getUser } = require('../../helpers/auth-helpers')
 const { localFileHandler } = require('../../helpers/file-helpers')
 const userController = {
@@ -8,23 +7,12 @@ const userController = {
     res.render('signup')
   },
   signUp: (req, res, next) => {
-    if (req.body.password !== req.body.passwordCheck) throw new Error('Passwords do not match!')
-
-    User.findOne({ where: { email: req.body.email } })
-      .then(user => {
-        if (user) throw new Error('Email already exists!')
-        return bcrypt.hash(req.body.password, 10)
-      })
-      .then(hash => User.create({
-        name: req.body.name,
-        email: req.body.email,
-        password: hash
-      }))
-      .then(() => {
-        req.flash('success_messages', '成功註冊帳號！')
-        res.redirect('/signin')
-      })
-      .catch(err => next(err))
+    userServices.signUp(req, (err, data) => {
+      if (err) next(err)
+      req.flash('success_messages', '成功註冊帳號！')
+      req.session.createdData = data
+      res.redirect('/signin')
+    })
   },
   signInPage: (req, res) => {
     res.render('signin')
