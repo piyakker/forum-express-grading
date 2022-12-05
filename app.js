57944f404b174da555ca7da9c6817b2119fda698
piyakker <ericlien86@gamil.com>
2022-12-05 19:56:11 +0800

    add api-auth middleware

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+
 const path = require('path')
 const express = require('express')
 const { pages, apis } = require('./routes')
@@ -8,10 +12,6 @@ const session = require('express-session')
 const passport = require('./config/passport')
 const { getUser } = require('./helpers/auth-helpers.js')
 const handlebarsHelpers = require('./helpers/handlebars-helpers')
-
-if (process.env.NODE_ENV !== 'production') {
-  require('dotenv').config()
-}
 
 const app = express()
 const port = process.env.PORT || 3000
