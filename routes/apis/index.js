ca619d2d0a1825684a50180313dc0f0c54a1fbe0
piyakker <ericlien86@gamil.com>
2022-12-06 11:51:42 +0800

    add api-postComment

--- routes/apis/index.js
+++ routes/apis/index.js
@@ -4,6 +4,7 @@ const passport = require('../../config/passport')
 const admin = require('./modules/admin')
 const restController = require('../../controllers/apis/restaurant-controller')
 const userController = require('../../controllers/apis/user-controller')
+const commentController = require('../../controllers/apis/comment-controller')
 const { authenticated, authenticatedAdmin } = require('../../middleware/api-auth')
 const { apiErrorHandler } = require('../../middleware/error-handler')
 
@@ -17,5 +18,8 @@ router.get('/restaurants/feeds', authenticated, restController.getFeeds)
 router.get('/restaurants/:id/dashboard', authenticated, restController.getDashboard)
 router.get('/restaurants/:id', authenticated, restController.getRestaurant)
 router.get('/restaurants', authenticated, restController.getRestaurants)
+
+router.post('/comments', authenticated, commentController.postComment)
+
 router.use('/', apiErrorHandler)
 module.exports = router
